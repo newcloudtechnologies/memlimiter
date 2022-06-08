cc0504f204c41477f400de6449a58a46404de97a
Vitaly Isaev <vitalyisaev2@gmail.com>
2022-06-08 15:16:52 +0300

    MLN-665 | feat: add copyright header

--- doc.go
+++ doc.go
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) New Cloud Technologies, Ltd. 2013-2022.
+ * Author: Vitaly Isaev <vitaly.isaev@myoffice.team>
+ * License: https://github.com/newcloudtechnologies/memlimiter/blob/master/LICENSE
+ */
+
 // Package memlimiter - memory budget control subsystem for Go services.
 // It tracks memory budget utilization and tries to stabilize memory usage with
 // backpressure (GC and request throttling) techniques.
--- utils/config/bytes/doc.go
+++ utils/config/bytes/doc.go
@@ -1,2 +1,8 @@
+/*
+ * Copyright (c) New Cloud Technologies, Ltd. 2013-2022.
+ * Author: Vitaly Isaev <vitaly.isaev@myoffice.team>
+ * License: https://github.com/newcloudtechnologies/memlimiter/blob/master/LICENSE
+ */
+
 // Package bytes helps to represent human-readable size values in JSON.
 package bytes
